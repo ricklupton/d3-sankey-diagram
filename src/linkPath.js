78276751c6c1b97afc07402f4c210c1c9a965e84
akshat sandhaliya <akshatsandhaliya@akshats-MacBook-Pro.local>
2023-10-09 08:54:56 +0530

    .

--- src/linkPath.js
+++ src/linkPath.js
@@ -138,7 +138,7 @@ export default function sankeyLink() {
     //   r0 = centerRadius;
     //   r1 = centerRadius;
     // }
-    var radius = centerRadius + (Math.abs(centerX - (x0 + x1) / 2) / ((x1 - x0) / 2)) * (endRadius - centerRadius);
+    // var radius = centerRadius + (Math.abs(centerX - (x0 + x1) / 2) / ((x1 - x0) / 2)) * (endRadius - centerRadius);
 
     function arc(dir, r) {
       var f = ( dir * (y1-y0) > 0) ? 1 : 0,
@@ -152,25 +152,25 @@ export default function sankeyLink() {
     // if (fx * (x2 - x3) < 0 || Math.abs(y1 - y0) > 4*h) {
     // XXX this causes juddering during transitions
 
-    // path =  ("M"     + [x0,    y0-h ] + " " +
-    //           arc(+1, r0) + [x2+hs, y2-hc] + " " +
-    //         "L"     + [x3+hs, y3-hc] + " " +
-    //           arc(-1, r1) + [x1,    y1-h ] + " " +
-    //         "L"     + [x1,    y1+h ] + " " +
-    //           arc(+1, r1) + [x3-hs, y3+hc] + " " +
-    //         "L"     + [x2-hs, y2+hc] + " " +
-    //           arc(-1, r0) + [x0,    y0+h ] + " " +
-    //         "Z");
+    path =  ("M"     + [x0,    y0-h ] + " " +
+              arc(+1, r0) + [x2+hs, y2-hc] + " " +
+            "L"     + [x3+hs, y3-hc] + " " +
+              arc(-1, r1) + [x1,    y1-h ] + " " +
+            "L"     + [x1,    y1+h ] + " " +
+              arc(+1, r1) + [x3-hs, y3+hc] + " " +
+            "L"     + [x2-hs, y2+hc] + " " +
+              arc(-1, r0) + [x0,    y0+h ] + " " +
+            "Z");
 
-    path = ("M" + [x0, y0 - h] + " " +
-          arc(+1, radius) + [centerX, y0] + " " +
-          "L" + [centerX, y1] + " " +
-          arc(+1, radius) + [x1, y1 - h] + " " +
-          "L" + [x1, y1 + h] + " " +
-          arc(-1, radius) + [centerX, y1] + " " +
-          "L" + [centerX, y0] + " " +
-          arc(-1, radius) + [x0, y0 - h] + " " +
-          "Z");
+    // path = ("M" + [x0, y0 - h] + " " +
+    //       arc(+1, radius) + [centerX, y0] + " " +
+    //       "L" + [centerX, y1] + " " +
+    //       arc(+1, radius) + [x1, y1 - h] + " " +
+    //       "L" + [x1, y1 + h] + " " +
+    //       arc(-1, radius) + [centerX, y1] + " " +
+    //       "L" + [centerX, y0] + " " +
+    //       arc(-1, radius) + [x0, y0 - h] + " " +
+    //       "Z");
 
     
     if (/NaN/.test(path)) {
