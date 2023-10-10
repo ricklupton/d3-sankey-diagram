1decee5ca17b9298d5a76ed71b0fe09e098d51c7
akshat sandhaliya <akshatsandhaliya@akshats-MacBook-Pro.local>
2023-10-10 17:19:45 +0530

    .

--- src/node.js
+++ src/node.js
@@ -143,7 +143,7 @@ export default function () {
         .text(titleText)
 
       value
-        .text("topic A 70%")
+        .html("topic A <span style='color:purple;'>70&</span>")
         .style('display', separateValue ? 'inline' : 'none')
         .style('fill', 'blue') // Set the font color to red
         .style('font-size', '12px'); 
