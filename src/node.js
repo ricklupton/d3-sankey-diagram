184fa30c3c8c4616368be3f917543d23e9d9d9f5
akshat sandhaliya <akshatsandhaliya@akshats-MacBook-Pro.local>
2023-10-10 15:18:43 +0530

    .

--- src/node.js
+++ src/node.js
@@ -170,7 +170,7 @@ export default function () {
       }
 
       // dropoff NOde
-      const dropoffOffset = (d.y1 - d.y0 )(1- d.dropoff/100);
+      const dropoffOffset = (d.y1 - d.y0 ) * (1- d.dropoff/100);
       console.log("dropofff", d.dropoff);
       dropoff
         .attr('transform', `translate(100, ${dropoffOffset})`)
